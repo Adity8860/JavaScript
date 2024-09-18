e1fbb9eeb9d7f1e2a6659e64337da58632476c64
Aditya Gaur <119625587+Adity8860@users.noreply.github.com>
2024-09-18 08:07:52 +0000

    conversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abs";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1 oR false =>0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 =>true; oR 0 => false
+// " " => false
+// "Aditya" => true
+
+let someNumber = 23  
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+
