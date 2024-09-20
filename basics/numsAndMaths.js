02ae5628cb3862a90452fd99824f7d83d3a6063c
Aditya Gaur <119625587+Adity8860@users.noreply.github.com>
2024-09-20 16:19:58 +0000

    Nums And Maths

---
+++ basics/numsAndMaths.js
@@ -0,0 +1,39 @@
+const score = 400;
+//  
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber= 223.8965
+
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 10000000
+
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+/**                       Maths                           */
+
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.8))
+// console.log(Math.floor(5.9))
+// console.log(Math.min(4,5,2,8))
+// console.log(Math.max(2,6,9,6))
+
+
+console.log(Math.random)
+console.log(Math.random()*10)
+console.log(Math.random()*10+1)
+console.log(Math.floor(Math.random()*10)+1)
+
+const min =10 
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min +1))+min)
