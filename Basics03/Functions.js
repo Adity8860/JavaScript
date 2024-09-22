7d54742400559d7640edb3224ac8e23886847596
Aditya Gaur <gauradi355@gmail.com>
2024-09-23 00:55:53 +0530

    commit

--- Basics03/Functions.js
+++ Basics03/Functions.js
@@ -49,3 +49,33 @@ function loginUserMessage(username) {
 
 console.log(loginUserMessage());
 // console.log(loginUserMessage("Aditya"));
+
+function calculateCartPrice(...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200,400,500))
+
+const user = {
+  username: "Aditya",
+  price: 199,
+};
+function handleObject(anyobject) {
+  console.log(
+    `username is ${anyobject.username} and price is ${anyobject.price}`
+  );
+}
+
+// handleObject(user)
+handleObject({
+  username: "Aditya",
+  price: 199,
+});
+
+const myNewArray  = [200, 400 , 100, 600]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 500, 1000]))
